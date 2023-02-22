1863e9f7a179a9ff56ab010db7ed5b1c4f4b4326
KhamilaNur <nasywahanif@gmail.com>
2023-02-22 15:04:54 +0700

    first commit

---
+++ app/src/main/java/com/example/vegetables/bawang.kt
@@ -0,0 +1,11 @@
+package com.example.vegetables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class bawang : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bawang)
+    }
+}
---
+++ app/src/main/java/com/example/vegetables/selada.kt
@@ -0,0 +1,11 @@
+package com.example.vegetables
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class selada : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_selada)
+    }
+}
